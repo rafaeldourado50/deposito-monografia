8e21dd5fd94e5a446ee720f28726c05cc9f021f8
Rafael Dourado <rafael.dourado810@gmail.com>
2018-11-10 18:30:58 -0300

    commit inicial

---
+++ src/main/java/br/ufba/depositomonografia/DepositoMonografiaApplication.java
@@ -0,0 +1,12 @@
+package br.ufba.depositomonografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositoMonografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositoMonografiaApplication.class, args);
+	}
+}
